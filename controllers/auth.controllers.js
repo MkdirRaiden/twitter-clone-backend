1806b98c5849edc875bf533498d65d3e81f6e7f1
Ahmed <1996raiden27@gmail.com>
2025-06-12 01:05:30 +0530

    cookie options updated

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -21,7 +21,7 @@ const generateTokenAndSetCookie = (payload, user, req, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "lax",
   });
 };
 
